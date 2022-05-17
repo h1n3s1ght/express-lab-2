a7a7a6e8753265b4a041374579c3b12603f3651a
h1n3s1ght <benjaminisaachines@gmail.com>
2022-05-17 07:39:07 -0500

    resets after 0

--- pass-it-around/index.js
+++ pass-it-around/index.js
@@ -1,7 +1,7 @@
 // Set the variables of express and app
 const express = require('express');
 const app = express();
-// let bottles = 100;
+let bottles = 100;
 let PORT = 3000;
 
 //set up active link
@@ -14,11 +14,10 @@ app.listen(PORT);
 // })
 
 app.get("/:number_of_bottles", (req, res) => {
-    while(req.params.number_of_bottles >= 0){
+    if (req.params.number_of_bottles > 0){
     console.log(req.params.number_of_bottles);
     res.send(`<html>
     <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <style>
     a {
         color: black;
@@ -34,6 +33,26 @@ app.get("/:number_of_bottles", (req, res) => {
 
     <body>
     <h1><strong>` + req.params.number_of_bottles + `</strong> Bottles of beers in the <strong>FRIDGE</strong> ...because why would they be on the wall?  Do you like warm beer?</h1> <br> <br> <a class="justWork" href='http://localhost:3000/`+ (req.params.number_of_bottles - 1 ) +`' style="text-decoration: none">Take one out, pass it around!</a> 
+    </body>
+    </html>`);
+}
+else {
+    res.send(`<html>
+    <head>
+    <style>
+    a {
+        color: black;
+        font-size: 1.5em;
+    }
+
+    a:hover {
+        color: white;
+        background-color: black;
+    }
+    </style>
+    </head>
+    <body>
+    <h1><strong>` + req.params.number_of_bottles + `</strong> Bottles of beers in the <strong>FRIDGE</strong> ...because you drank them all!</h1> <br> <br> <a class="justWork" href='http://localhost:3000/`+ 100 +`' style="text-decoration: none">Refill the beer here.</a> 
     </body>
     </html>`);
 }
